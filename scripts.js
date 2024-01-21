950418946deb1510e47957f7807a1c615014b98b
alicemorey <alicemmorey@gmail.com>
2024-01-21 21:19:51 +0100

    Updated Pokemon List with objects

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
-alert('Hello world');
-
-let favoriteFood= 'apple';
-document.write(favouriteFood);
-
-
+let pokemonList = [
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['grass', 'electric'],
+        abilities: ['Rain-dish','Torrent']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1 ,
+        type: ['rock','water'],
+        abilities: ['Blaze','Solar-power']
+    },
+    {
+        name: 'Caterpie',
+        height:0.3 ,
+        type: ['flying','fire'],
+        abilities: ['shield-dust','run-away']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4 ,
+        type: ['electric','ground'],
+        abilities: ['static','lightningrod']
+    }
+]
