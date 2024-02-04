5b0a8c52b701a56512c08ade7545e610d479ab36
alicemorey <alicemmorey@gmail.com>
2024-02-04 22:25:26 +0100

    addListItem

--- scripts.js
+++ scripts.js
@@ -42,9 +42,20 @@ function add (pokemon) {
  function getAll() {
     return repository;
  }
+
+ function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement ("li");
+    let button =document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild (listpokemon);
+ }
  return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem
  }; 
 })();
 
@@ -54,13 +65,8 @@ pokemonRepository.add ({name: "Pikachu", height: 0.3, types: ["electric"]});
 console.log (pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach (function (pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement ("li");
-    let button =document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild (listpokemon);
+   
+    pokemonRepository.addListItem(pokemon);
 });
 
 
