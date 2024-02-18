7bad677d1647b770d456e2af52a3680fa523484f
alicemorey <alicemmorey@gmail.com>
2024-02-18 18:32:36 +0100

    adding modals

--- scripts.js
+++ scripts.js
@@ -1,90 +1,141 @@
-
 let pokemonRepository = (function () {
-    let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+ //show details of Pokemon using Modals
+ let modalContainer = document.querySelector('#modal-container');
+ function showModal(title, text, img) {
+  modalContainer.innerHTML= '';
+
+  let modal= document.createElement ('div');
+  modal.classList.add ('modal');
+
+  let closeButtonElement =document.createElement ('button');
+  closeButtonElement.classList.add ('modal-close');
+  closeButtonElement.innerText= 'close';
+  closeButtonElement.addEventListener ('click',hideModal);
+
+  let titleElement= document.createElement ('h1');
+  titleElement.innerText= title;
+
+  let contentElement = document.createElement ('p');
+  contentElement.innerText = text;
+
+  let imageElement =document.createElement ("img");
+  imageElement.setAttribute("src", img);
+  imageElement.setAttribute ("width", "30");
+  imageElement.setAttribute ("height", "30");
+  imageElement.setAttribute ("alt", "Pokemon image");
+
+  modal.appednChild (closeButtonElement);
+  modal.appendChild (titleElement);
+  modal.appendChild (contentElement);
+  modal.appendChild (imageElement);
+  modalContainer.appendChild (modal);
+
+  modalContainer.classList.add ('is-visible');
+ }
+
+ function hideModal(){
+  modalContainer.classList.remove ('is-visible');
+ }
+
+window.addEventListener('keydown', (e)=> {
+  if (e.key === 'Escape' &&
+  modalContainer.classList.contains ('is-visible')){
+    hideModal();
+  }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
+function add(item){
+  pokemonList.push (item);
+}
+
+
 function add (pokemon) {
- if ( 
-    typeof pokemon === 'object'&&
-    "name" in pokemon &&
-    "detailsUrl" in pokemon
- ) {
-    pokemonList.push (pokemon);
- } else {
-    console.log ("pokemon is not correct")
+if ( 
+  typeof pokemon === 'object'&&
+  "name" in pokemon &&
+  "detailsUrl" in pokemon
+) {
+  pokemonList.push (pokemon);
+} else {
+  console.log ("pokemon is not correct")
+  }
+}
+function getAll() {
+  return pokemonList;
+}
+function addListItem(pokemon){
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listpokemon = document.createElement ("p");
+  let button =document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listpokemon.appendChild(button);
+  pokemonList.appendChild (listpokemon);
+  button.addEventListener ('click', function (event){
+      showDetails (pokemon);
+      });
+  }
+  function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+            height: item.height,
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
     }
- }
- function getAll() {
-    return pokemonList;
- }
- function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement ("p");
-    let button =document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild (listpokemon);
-    button.addEventListener ('click', function (event){
-        showDetails (pokemon);
-        });
+
+function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+function showDetails(item) {
+  pokemonRepository.loadDetails(item).then(function () {
+    showModal(pokemon)
+      });
     }
-    function loadList() {
-        return fetch(apiUrl).then(function (response) {
-          return response.json();
-        }).then(function (json) {
-          json.results.forEach(function (item) {
-            let pokemon = {
-              name: item.name,
-              detailsUrl: item.url,
-              height: item.height,
-            };
-            add(pokemon);
-            console.log(pokemon);
-          });
-        }).catch(function (e) {
-          console.error(e);
-        })
-      }
-    
-    function loadDetails(item) {
-        let url = item.detailsUrl;
-        return fetch(url).then(function (response) {
-          return response.json();
-        }).then(function (details) {
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
-          item.types = details.types;
-        }).catch(function (e) {
-          console.error(e);
-        });
-      }
-    
-      function showDetails(item) {
-        pokemonRepository.loadDetails(item).then(function () {
-          console.log(item);
-        });
-      }
 
-
- return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails,
- }; 
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  showDetails: showDetails,
+  addListItem: addListItem
+}; 
 })();
 
+pokemonRepository.add({ name: 'bulbasaur', height: 1, logo: "img/001.png" });
 
 pokemonRepository.loadList().then (function(){
 pokemonRepository.getAll().forEach (function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.addListItem(pokemon);
 });
-});
-
-var img= document.createElement ("img");
-    img. src="001.png";
-    var src=document.getElementByID ("x");
-    src.appemdChild(img);
+})
